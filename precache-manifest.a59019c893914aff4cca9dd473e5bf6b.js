46e52c2245e8c0ab4026da76341860beedf5ba47
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-07-11 13:47:20 +0800

    Updates

--- precache-manifest.a59019c893914aff4cca9dd473e5bf6b.js
+++ precache-manifest.a59019c893914aff4cca9dd473e5bf6b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d7acc8da81467d35548366b7e6c463db",
+    "revision": "c267bc6e14f39936139e0cc3954cdbb5",
     "url": "/weatherino-react/index.html"
   },
   {
-    "revision": "5006295a8849b6f52cda",
+    "revision": "80c5b0c499dbd62065d6",
     "url": "/weatherino-react/static/css/main.2b3c136f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weatherino-react/static/js/2.8c4d3189.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5006295a8849b6f52cda",
-    "url": "/weatherino-react/static/js/main.2f1201b1.chunk.js"
+    "revision": "80c5b0c499dbd62065d6",
+    "url": "/weatherino-react/static/js/main.29d9296e.chunk.js"
   },
   {
     "revision": "cf8d8edc397242909c8d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weatherino-react/precache-manifest.73e42495803b7d0655b2413e7c65ce23.js"
+  "/weatherino-react/precache-manifest.a59019c893914aff4cca9dd473e5bf6b.js"
 );
 
 self.addEventListener('message', (event) => {
